a7167bd08463c74e8e66d698e98bbdebc34a52be
tiger <thisisstreamliner@gmail.com>
2024-03-14 01:10:42 +0200

    Fetch Accounts From Ganache

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,3 +3,21 @@ const ganache = require('ganache');
 const {Web3} = require('web3');
 
 const web3 = new Web3(ganache.provider());
+
+beforeEach(() => {
+    // Get a list of all accounts
+    web3.eth.getAccounts()
+    .then(fetchedAccounts => {
+        console.log(fetchedAccounts);
+    });
+
+    // Use one of those accounts to deploy
+    // the contract
+
+});
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        
+    });
+});
