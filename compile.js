a49199047df0310f47fde7ec037744ca5be7bf42
tiger <thisisstreamliner@gmail.com>
2024-03-15 00:50:18 +0200

    fix: deploying contract. feat: assertion deployment

--- compile.js
+++ compile.js
@@ -21,10 +21,18 @@ var input = {
     }
 }; 
 
-var output = JSON.parse(solc.compile(JSON.stringify(input)));
+// Compile the Solidity code using solc
+const compiledCode = JSON.parse(solc.compile(JSON.stringify(input)));
 
-module.exports = output.contracts['Inbox.sol']['Inbox']
+// Get the bytecode from the compiled contract
+const bytecode = compiledCode.contracts['Inbox.sol']['Inbox'].evm.bytecode.object;
 
+const abi = compiledCode.contracts['Inbox.sol']['Inbox'].abi;
+
+module.exports = { abi, bytecode };
+
+// Log the Contract ABI to the console
+console.log('Contract ABI:\n', abi);
 //console.log(output.contracts['Inbox.sol']['Inbox']);
 
 // `output` here contains the JSON output as specified in the documentation
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,25 +4,42 @@ const {Web3} = require('web3');
 
 const web3 = new Web3(ganache.provider());
 
-const {interface, bytecode} = require('../compile');
+const {abi, bytecode} = require('../compile');
 
 let accounts;
-let inbox;
+let myContract;
+let tx;
 
 beforeEach(async () => {
     // Get a list of all accounts
     accounts = await web3.eth.getAccounts();
+    const defaultAccount = accounts[0];
+    console.log('deployer account:', defaultAccount);
 
     // Use one of those accounts to deploy
     // the contract
-    inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: ['Hi there!'] })
-        .send({ from: accounts[0], gas: '1000000' });
+    myContract = new web3.eth.Contract(abi);
+    myContract.handleRevert = true;
+    const contractDeployer = myContract.deploy({
+		data: '0x' + bytecode,
+		arguments: ['Hello World'],
+	});
+    const gas = await contractDeployer.estimateGas({
+		from: defaultAccount,
+	});
+	console.log('estimated gas:', gas);
+    tx = await contractDeployer.send({
+        from: defaultAccount,
+        gas,
+        gasPrice: 10000000000,
+    });
+    console.log('Contract deployed at address: ' + tx.options.address);
 
 });
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(inbox);
+        assert.ok(tx.options.address);
+        console.log(myContract);
     });
 });
