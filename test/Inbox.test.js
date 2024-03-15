15bb4e5b1191210c9c7a3ed5d86188bd67fe0da7
tiger <thisisstreamliner@gmail.com>
2024-03-15 14:38:10 +0200

    feat: add more tests

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -13,26 +13,25 @@ let tx;
 beforeEach(async () => {
     // Get a list of all accounts
     accounts = await web3.eth.getAccounts();
-    const defaultAccount = accounts[0];
-    console.log('deployer account:', defaultAccount);
+    const deployerAccount = accounts[0];
+    console.log('Deployer account:', deployerAccount);
 
     // Use one of those accounts to deploy
     // the contract
     myContract = new web3.eth.Contract(abi);
-    myContract.handleRevert = true;
     const contractDeployer = myContract.deploy({
 		data: '0x' + bytecode,
 		arguments: ['Hello World'],
 	});
-    const gas = await contractDeployer.estimateGas({
-		from: defaultAccount,
-	});
-	console.log('estimated gas:', gas);
+    const gas = await contractDeployer.estimateGas({from: deployerAccount});
+	console.log('Estimated gas:', gas);
+
     tx = await contractDeployer.send({
-        from: defaultAccount,
+        from: deployerAccount,
         gas,
-        gasPrice: 10000000000,
+        gasPrice: '10000000000',
     });
+    myContract.options.address = tx.options.address;
     console.log('Contract deployed at address: ' + tx.options.address);
 
 });
@@ -42,4 +41,17 @@ describe('Inbox', () => {
         assert.ok(tx.options.address);
         console.log(myContract);
     });
+
+    it('has a default message', async () => {
+        const message = await myContract.methods.message().call();
+        assert.equal(message, 'Hello World');
+    });
+
+    it('can change the message', async () => {
+        await myContract.methods.setMessage('Buy Worlds').send({ from: accounts[0] });
+        const newMessage = await myContract.methods.message().call();
+        assert.equal(newMessage, 'Buy Worlds');
+    });
+
+    
 });
