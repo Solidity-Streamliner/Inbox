193db275d08ff4503eb111ad2999e95d2283d8dd
tiger <thisisstreamliner@gmail.com>
2024-03-17 11:26:58 +0200

    chore: setup infura

--- deploy.js
+++ deploy.js
@@ -1,3 +1,12 @@
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 const Web3 = require("web3");
-const {abi, bytecode} = require('compile');
+const {abi, bytecode} = require('./compile');
+
+let provider = new HDWalletProvider({
+    mnemonic: {
+        phrase: "nasty eyebrow silent squeeze put edge protect song fox rigid among repair"
+    },
+    providerOrUrl: "https://goerli.infura.io/v3/e3b36f9afbdd46e2aaf70dacd1a00c04"
+});
+
+const web3 = new Web3(provider);
